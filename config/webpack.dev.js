79a95278f0fd166a6326b2a051735a6f7be0948e
David-qiuwenhui <1059713136@qq.com>
2023-05-07 16:23:59 +0800

    save

---
+++ config/webpack.dev.js
@@ -0,0 +1,81 @@
+const path = require("path");
+const ESLintWebpackPlugin = require("eslint-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    // 入口文件
+    entry: "./src/main.js",
+    // 输出
+    output: {
+        // path: 文件输出目录（绝对路径）
+        path: undefined,
+        // filename: 输出文件名
+        filename: "static/js/main.js",
+        // clean: true, // 自动将path目录的资源清空
+    },
+    // loader
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                // 执行顺序：从右到左 从下到上
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.less$/,
+                use: ["style-loader", "css-loader", "less-loader"],
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.styl$/,
+                use: ["style-loader", "css-loader", "stylus-loader"],
+            },
+            {
+                test: /\.(png|jpe?g|gif|webp)$/,
+                type: "asset",
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 1 * 1024,
+                    },
+                },
+                generator: {
+                    filename: "static/imgs/[hash:8][ext][query]",
+                },
+            },
+            {
+                test: /\.(ttf|woff2?|map4|map3|avi)$/,
+                type: "asset/resource",
+                generator: {
+                    filename: "static/media/[hash:8][ext][query]",
+                },
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+            },
+        ],
+    },
+    plugins: [
+        new ESLintWebpackPlugin({
+            // 指定检查文件的根目录
+            context: path.resolve(__dirname, "../src"),
+        }),
+        new HtmlWebpackPlugin({
+            // 以 public/index.html 为模板创建文件
+            // 新的html文件有两个特点：1. 内容和源文件一致 2. 自动引入打包生成的js等资源
+            template: path.resolve(__dirname, "../public/index.html"),
+        }),
+    ],
+    // 开发服务器
+    devServer: {
+        host: "localhost", // 启动服务器域名
+        port: "3000", // 启动服务器端口号
+        open: true, // 是否自动打开浏览器
+    },
+    // 开发模式
+    mode: "development",
+};
---
+++ config/webpack.prod.js
@@ -0,0 +1,105 @@
+const path = require("path");
+const ESLintWebpackPlugin = require("eslint-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+const getStyleLoaders = (preProcessor) => {
+    return [
+        MiniCssExtractPlugin.loader,
+        "css-loader",
+        {
+            loader: "postcss-loader",
+            options: {
+                postcssOptions: {
+                    plugins: ["postcss-preset-env"],
+                },
+            },
+        },
+        preProcessor,
+    ].filter(Boolean);
+};
+
+module.exports = {
+    // 入口文件
+    entry: "./src/main.js",
+    // 输出
+    output: {
+        // path: 文件输出目录（绝对路径）
+        path: path.resolve(__dirname, "../dist"),
+        // filename: 输出文件名
+        filename: "static/js/main.js",
+        clean: true, // 自动将path目录的资源清空
+    },
+    // loader
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                // 执行顺序：从右到左 从下到上
+                use: getStyleLoaders(),
+            },
+            {
+                test: /\.less$/,
+                use: getStyleLoaders("less-loader"),
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: getStyleLoaders("sass-loader"),
+            },
+            {
+                test: /\.styl$/,
+                use: getStyleLoaders("stylus-loader"),
+            },
+            {
+                test: /\.(png|jpe?g|gif|webp)$/,
+                type: "asset",
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 1 * 1024,
+                    },
+                },
+                generator: {
+                    filename: "static/imgs/[hash:8][ext][query]",
+                },
+            },
+            {
+                test: /\.(ttf|woff2?|map4|map3|avi)$/,
+                type: "asset/resource",
+                generator: {
+                    filename: "static/media/[hash:8][ext][query]",
+                },
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+            },
+        ],
+    },
+    plugins: [
+        new ESLintWebpackPlugin({
+            // 指定检查文件的根目录
+            context: path.resolve(__dirname, "../src"),
+        }),
+        new HtmlWebpackPlugin({
+            // 以 public/index.html 为模板创建文件
+            // 新的html文件有两个特点：1. 内容和源文件一致 2. 自动引入打包生成的js等资源
+            template: path.resolve(__dirname, "../public/index.html"),
+        }),
+        // 提取 CSS 成单独文件
+        new MiniCssExtractPlugin({
+            filename: "static/css/main.css",
+        }),
+        // CSS 体积压缩
+        new CssMinimizerPlugin(),
+    ],
+    // 开发服务器
+    /*     devServer: {
+        host: "localhost", // 启动服务器域名
+        port: "3000", // 启动服务器端口号
+        open: true, // 是否自动打开浏览器
+    }, */
+    // 生产模式
+    mode: "production",
+};
